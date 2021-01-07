9d1c0da469e0dc730d089d332e507fc8dd2f91e8
claytonschoonoversmith@gmail.com <claytonschoonoversmith@gmail.com>
2021-01-06 19:44:02 -0600

    used mapStateToProps to extract data we want to UserHeader component successfully

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,12 +1,13 @@
 import jsonPlaceholder from '../apis/jsonPlaceholder';
 
-export const fetchPosts = () => {
-	//Bad approach!!!!
-	const promise = jsonPlaceholder.get('/posts');
+export const fetchPosts = () => async (dispatch) => {
+	const response = await jsonPlaceholder.get('/posts');
 
+	dispatch({ type: 'FETCH_POSTS', payload: response.data })
+};
 
-	return {
-		type: 'FETCH_POSTS',
-		payload: promise
-	};
+export const fetchUser = (id) => async dispatch => { 
+	const response = await jsonPlaceholder.get(`/users/${id}`);
+
+	dispatch({ type: 'FETCH_USER', payload: response.data });
 };
